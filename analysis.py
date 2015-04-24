c6b232c650f915b3c1941068dd6f14398bcda72c
rjuchnicki <robert.juchnicki@gmail.com>
2015-04-24 01:56:12 -0400

    basic business statistics

--- analysis.py
+++ analysis.py
@@ -61,8 +61,8 @@ def unique_vals(db, field):
 	res = []
 
 	for key in db:
-		if key[db][field] not in res:
-			res.append(key[db][field])
+		if db[key][field] not in res:
+			res.append(db[key][field])
 
 	return res
 
@@ -72,8 +72,8 @@ def unique_years(db, date_field):
 	res = []
 
 	for key in db:
-		if key[db][date_field] not in res:
-			res.append(key[db][date_field][0:4])
+		if db[key][date_field] not in res:
+			res.append(db[key][date_field][0:4])
 
 	return res.sorted()
 
@@ -89,6 +89,10 @@ if __name__ == "__main__":
 	users = cPickle.load(f)
 	f.close()
 
+	print "-----------------"
+	print " USER STATISTICS "
+	print "-----------------"
+	print
 
 	num_users = len(users.keys())
 	print "Number of users in database:", num_users, '\n'
@@ -99,9 +103,9 @@ if __name__ == "__main__":
 	print "75-th Percentile Review Count:", db_percentile(users, 'review_count', 75), '\n'
 
 	print "Average Average Star Rating:", db_average(users, 'average_stars')
-	print "25-th Percentile Average Stars:", db_percentile(users, 'review_count', 25)
-	print "50-th Percentile Review Count:", db_percentile(users, 'review_count', 50)
-	print "75-th Percentile Review Count:", db_percentile(users, 'review_count', 75), '\n'
+	print "25-th Percentile Average Stars:", db_percentile(users, 'average_stars', 25)
+	print "50-th Percentile Review Count:", db_percentile(users, 'average_stars', 50)
+	print "75-th Percentile Review Count:", db_percentile(users, 'average_stars', 75), '\n'
 
 	num_elites = 0
 	current_elites = 0
@@ -122,11 +126,49 @@ if __name__ == "__main__":
 			yelping_since[int(users[user]['yelping_since'][0:4])] += 1
 
 	print "Number of users that were elite at some point:", num_elites
-	print "Number of sers who are currently elite users:", current_elites
+	print "Number of users who are currently elite users:", current_elites
 	print "All years any users were elite:", all_elite_years, '\n'
 
 	print "Statistics for year joining Yelp"
 	for year in yelping_since:
 		print str(year) + ':', yelping_since[year], 'users'
 
-	# print "Number of elite:", elite_users
+	print '\n\n'
+
+	users.clear()
+
+	f = open('db_pickled' + slash + 'business_db_pickled')
+	businesses = cPickle.load(f)
+	f.close()
+	
+	print "---------------------"
+	print " BUSINESS STATISTICS "
+	print "---------------------"
+	print
+
+	num_businesses = len(businesses.keys())
+	print "Number of businesses in database:", num_businesses
+
+	opened = 0
+	categories = []
+	for b in businesses:
+		if businesses[b]['open'] == 'True':
+			opened += 1
+
+	print "Number of businesses that are open:", opened
+	print "Number of businesses that are closed:", num_businesses - opened, '\n'
+
+	print "Average Review Count:", db_average(businesses, 'review_count')
+	print "25-th Percentile Review Count:", db_percentile(businesses, 'review_count', 25)
+	print "50-th Percentile Review Count:", db_percentile(businesses, 'review_count', 50)
+	print "75-th Percentile Review Count:", db_percentile(businesses, 'review_count', 75), '\n'
+
+	print "Average Average Star Rating:", db_average(businesses, 'stars')
+	print "25-th Percentile Average Stars:", db_percentile(businesses, 'stars', 25)
+	print "50-th Percentile Review Count:", db_percentile(businesses, 'stars', 50)
+	print "75-th Percentile Review Count:", db_percentile(businesses, 'stars', 75), '\n'
+
+	print "Number of states businesses are in:", len(unique_vals(businesses, 'state'))
+	print "Number of cities businesses are in:", len(unique_vals(businesses, 'city')), '\n'
+
+	#print "Number of categories for businesses:"
